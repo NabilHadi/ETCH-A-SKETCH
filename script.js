fb80cdd9fa0e08f109cb4eeb89fa333973542ace
Nabil Hadi <42745851+NabilHadi@users.noreply.github.com>
2024-04-08 06:22:17 +0200

    Add create new grid button

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
 
-let numOfSquaresPerSide = 20;
-let numOfSquares = numOfSquaresPerSide * numOfSquaresPerSide;
-let squareflexPercentage = 100 / numOfSquaresPerSide;
-console.log(squareflexPercentage);
-for (let i = 0; i < numOfSquares; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.textContent = i;
-  square.style.flex = `0 0 ${squareflexPercentage}%`;
-  square.addEventListener("mouseenter", handleMouseEnter);
-  container.appendChild(square);
-}
+const newGridBtn = document.createElement("button");
+newGridBtn.textContent = "Create New Grid";
+newGridBtn.addEventListener("click", () => {
+  let numOfSquaresPerSide = Number(prompt("Enter number of squares per side of the grid (max is 100)"));
+  if (numOfSquaresPerSide > 100) {
+    alert("Please enter number less than 100");
+  } else if (numOfSquaresPerSide < 0 || Number.isNaN(numOfSquaresPerSide)) {
+    alert("Please enter a number between 0 and 100");
+  } else {
+    drawGrid(numOfSquaresPerSide);
+  }
+});
+body.appendChild(newGridBtn);
+body.appendChild(container);
 
 function handleMouseEnter(e) {
   e.target.style.backgroundColor = "black";
 }
+
+function drawGrid(numOfSquaresPerSide) {
+  document.querySelectorAll(".square").forEach((square) => {
+    square.remove();
+  });
+  let numOfSquares = numOfSquaresPerSide * numOfSquaresPerSide;
+  let squareflexPercentage = 100 / numOfSquaresPerSide;
+  for (let i = 0; i < numOfSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.flex = `0 0 ${squareflexPercentage}%`;
+    square.addEventListener("mouseenter", handleMouseEnter);
+    container.appendChild(square);
+  }
+}
+
+drawGrid(15);
