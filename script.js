4b217b94f059f454396fa62d3393ce5f840cea78
Nabil Hadi <42745851+NabilHadi@users.noreply.github.com>
2024-04-08 06:10:26 +0200

    Add hover effect on squres

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const container = document.querySelector("#container");
+
+let numOfSquaresPerSide = 20;
+let numOfSquares = numOfSquaresPerSide * numOfSquaresPerSide;
+let squareflexPercentage = 100 / numOfSquaresPerSide;
+console.log(squareflexPercentage);
+for (let i = 0; i < numOfSquares; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.textContent = i;
+  square.style.flex = `0 0 ${squareflexPercentage}%`;
+  square.addEventListener("mouseenter", handleMouseEnter);
+  container.appendChild(square);
+}
+
+function handleMouseEnter(e) {
+  e.target.style.backgroundColor = "black";
+}
